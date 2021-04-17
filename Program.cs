e1e79e6892e2aa3962b82d85d5d984d3420ab411
User-42 <a.t.iafrate@gmail.com>
2021-04-17 09:38:59 -0400

    worked out some bugs on levels, not totally fixed

--- Program.cs
+++ Program.cs
@@ -8,63 +8,94 @@ namespace guessing_game
         {
 
             Console.WriteLine("Let's play a guessing game!");
-            Console.WriteLine("I have a secret! It's a number. Can you guess the secret number?");
+            Console.Write("Choose a level:");
+            Console.Write("easy, medium, or hard.  ");
+            string level = Console.ReadLine();
+            int allowedTries;
 
-            int secretNumber = new Random().Next(1, 100);
-            string guess = Console.ReadLine();
-            int guessValue;
-            bool success = int.TryParse(guess, out guessValue);
-
-
-            if (success)
+            if (level == "easy")
             {
-                int allowedTries = 4;
-                int numberOfTries = 0;
-
-                while (true)
-                {
-                    numberOfTries++;
-
-                    if (guessValue > secretNumber)
-                    {
-                        Console.WriteLine($"Nope, Too high! Try again! You have {allowedTries - numberOfTries}  tries left.");
-                        guess = Console.ReadLine();
-                        success = int.TryParse(guess, out guessValue);
-                    }
-                    if (guessValue < secretNumber)
-                    {
-                        Console.WriteLine($"Nope, too low! Try again! You have {allowedTries - numberOfTries} tries left.");
-                        guess = Console.ReadLine();
-                        success = int.TryParse(guess, out guessValue);
-                    }
-                
-                    if (guessValue == secretNumber)
-                    {
-                        Console.WriteLine($"The answer was {secretNumber}! How'd you know that?");
-                        break;
-                    }
-                    if (numberOfTries == allowedTries -1)
-                    {
-                        Console.WriteLine($"I'm sorry, you've run out of tries! The number was {secretNumber}");
-                        break;
-                    }
-
-                }
-
+                allowedTries = 8;
+            }
+            else if (level == "medium")
+            {
+                allowedTries = 6;
+            }
+            else if (level == "hard")
+            {
+                allowedTries = 4;
             }
             else
             {
-                Console.WriteLine($"Numbers only, please.");
+
+                Console.WriteLine("Not a choice. Game over.");
+                allowedTries = 0;
+            }
+
+
+            Console.WriteLine("I have a secret! It's a number. Can you guess the secret number?");
+            int secretNumber = new Random().Next(1, 100);
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+
+            int numberOfTries = 0;
+
+            while (true)
+            {
+                
+                numberOfTries++;
+
+                if (numberOfTries == allowedTries)
+                {
+                    Console.WriteLine($"I'm sorry, you've run out of tries! The number was {secretNumber}.");
+                    break;
+                }
+
+                else if (guess > secretNumber)
+                {
+
+                    Console.WriteLine($"Nope, Too high! Try again! You have {allowedTries - numberOfTries}  tries left.");
+                    guess = Convert.ToInt32(Console.ReadLine());
+
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine($"Nope, too low! Try again! You have {allowedTries - numberOfTries} tries left.");
+                    guess = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+                else if (guess == secretNumber)
+                {
+                    Console.WriteLine($"The answer was {secretNumber}! How'd you know that?");
+                    break;
+                }
+
+
             }
 
         }
 
+
     }
+
 }
 
-//  string level = Console.ReadLine();
-//             if (level != "E" && level != "M" && level != "D")
-//             {
-//                 Console.WriteLine("Invalid response. Which level do you want? E, M, or D?");
-//                 level = Console.ReadLine();
-//             }
+
+
+
+
+// if (success)
+
+// int allowedTries = 4;
+// string guess = Console.ReadLine();
+// int guessValue;
+// bool success = int.TryParse(guess, out guessValue);
+// string level = "E";
+
+// else
+// {
+//     Console.WriteLine("Sorry, that's not a number. Game over!");
+// }
+// success = int.TryParse(guess, out guessValue);
+// success = int.TryParse(guess, out guessValue);
